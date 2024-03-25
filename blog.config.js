8e7baf0a4b20b5cc48dc03dc8bc0082c3fbf7873
Doubletreelin <doubletree.lin@foxmail.com>
2024-03-25 11:12:36 +0800

    提交

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Doubletree', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个认真搞钱的干饭人🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'http://myblog.doubletree.fun', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.doubletree.fun', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Doubletree, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
